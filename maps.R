9b7c074b95d3270fd30fa07336a2fd4063622e14
Madeline <mross15@uw.edu>
2018-11-14 22:39:52 -0800

    Add files

--- maps.R
+++ maps.R
@@ -6,22 +6,26 @@
 ### The file must be executable on the server!
 ### I.e. someone else must be able to just run it with 'Rscript maps.R'
 ### /on server/ and get the correct output.
-
-
+## install.packages("dplyr")
+## install.packages("ggplot2")
+library(dplyr)
+library(ggplot2)
 ## read the data
-##
+temp_data <- data.table::fread('temp_prec_middle_encrypted.csv.bz2', stringsAsFactors = FALSE)
+temp_data <- decrypt::decryptData(temp_data)
+## temp_tiny <- read.csv("temp_prec_tiny.csv.bz2", sep = "", stringsAsFactors = FALSE)
 ## hint1: figure out the correct format and use the correct function.
 ##
 ## hint2: read.table and friends are slow (about 6 min to read data).
 ## You may use data.table::fread instead (a few seconds)
-
+## data.frame(head(temp_data))
 
 ## filter out North American observations
-
+north_america_data <- filter(temp_data, longitude >= 100, latitude <=100)
 
 ## delete the original (large data) from R workspace
 ## this is necessary to conserve memory.
-
+rm(temp_data)
 
 ## -------------------- do the following for 1960, 1987, 2014 and temp/precipitation --------------------
 
@@ -36,6 +40,30 @@
 ## Warning: map projections may be slow (several minutes per plot).
 
 ## close the device
+## 1960
+data_1960 <- filter(north_america_data, time == "1960-06-01")
+jpeg(filename = "map1960_precipitation.jpg", width = 1000, height = 1000)
+ggplot(data_1960) + geom_point(mapping = aes(x = longitude, y = latitude, col = precipitation, size = 4)) + labs(title = "Precipiation in June 1960") + coord_map()
+dev.off()
+jpeg(filename = "map1960_airtemp.jpg", width = 1000, height = 1000)
+ggplot(data_1960) + geom_point(mapping = aes(x = longitude, y = latitude, col = airtemp, size = 4)) + labs(title = "Air Temperature in June 1960") + coord_map()
+dev.off()
 
+## 1986
+data_1986 <- filter(north_america_data, time == "1986-06-01")
+jpeg(filename = "map1986_precipitation.jpg", width = 1000, height = 1000)
+ggplot(data_1986) + geom_point(mapping = aes(x = longitude, y = latitude, col = precipitation, size = 4)) + labs(title = "Precipitation in June 1986") + coord_map() 
+dev.off()
+jpeg(filename = "map1986_airtemp.jpg", width = 1000, height = 1000)
+ggplot(data_1986) + geom_point(mapping = aes(x = longitude, y = latitude, col = airtemp, size = 4)) + labs(title = "Air Temperature in June 1986") + coord_map()
+dev.off()
+
+## 2014
+data_2014 <- filter(north_america_data, time == "2014-06-01")
+jpeg(filename = "map2014_precipitation.jpg", width = 1000, height = 1000)
+ggplot(data_2014) + geom_point(mapping = aes(x = longitude, y = latitude, col = precipitation, size = 4)) + labs(title = "Precipitation in June 2014") + coord_map() 
+dev.off()
+jpeg(filename = "map2014_airtemp.jpg", width = 1000, height = 1000)
+ggplot(data_2014) + geom_point(mapping = aes(x = longitude, y = latitude, col = airtemp, size = 4)) + labs(title = "Air Temperature in June 2014") + coord_map() 
+dev.off()
 ## -------------------- you are done.  congrats :-) --------------------
-
--- session.sh
+++ session.sh
@@ -12,29 +12,35 @@
 #
 # -------------------- local commands --------------------
 # 1. create a directory for this project.
-
+mkdir graphs 
 # 2. log onto the server
-
+ssh mross15@info201.ischool.uw.edu
 # 3. copy the small data subset from the server to your local machine
-
+scp /opt/data/temp_prec_middle_encrypted.csv.bz2 mross15@info201.ischool.uw.edu:
 # 4. copy your R-script to the server
-
+scp maps.R mross15@info201.ischool.uw.edu
 # 5. (after succesfully running the script remotely): copy the output files back to your computer
-
+map1960_airtemp.jpg
+map1960_precipitation.jpg
+map1986_airtemp.jpg
+map1986_precipitation.jpg
+map2014_airtemp.jpg
+map2014_precipitation.jpg
 # 6. inspect that the copy was successful
-
+ls 
 
 # -------------------- remote commands --------------------
 # 1. explore the data directory '/opt/data'.  How do you find out the size of the files?
-
+temp_prec_middle_encrypted.csv.bz2 
 # 2. explore the first few lines of the small sample data
-
+temp_data <- data.table::fread('temp_prec_middle_encrypted.csv.bz2', stringsAsFactors = FALSE)
 # 3. create a directory for this project
-
+mkdir assignment6
 # 4. (after copying your code from the laptop): inspect the files in the project directory
-
+ls - l
 # 5. run your script
-
+Rscript maps.R
 # 6. How do you install missing R packages?
-
+install.packages("utils")
 # 7. (after running the script): inspect the folder for output files
+ls
